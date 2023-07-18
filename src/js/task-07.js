7f63d34aef38f340580f17e4e3003ce045fcf562
Horbunov Yurii <urijnet@gmail.com>
2023-07-18 17:44:02 +0300

    tasck9

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,16 +1,43 @@
 const inputRef = document.querySelector("#font-size-control");
 const titleRef = document.querySelector("#text");
 
-// inputRef.addEventListener('input', onRangeChange);
+inputRef.addEventListener('input', onRangeChange);
 
-inputRef.addEventListener("input", function () {
-  titleRef.style.fontSize = `${inputRef.value}px`;
-});
-// function onRangeChange(evt){
-// const value = evt.currentTarget.value;
-// const text = titleRef.textContent;
-//   text.style.fontSize = `${value}px`;
-   
-// }
+
+function onRangeChange(evt){
+const value = evt.currentTarget.value;
+
+  titleRef.style.fontSize = `${value}px`;
+}
+
+
+
+// inputRef.addEventListener("input", function () {
+//   titleRef.style.fontSize = `${inputRef.value}px`;
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", onFormSabmit);
+
+
+function onFormSabmit(evt){
+  evt.preventDefault();
+ 
+  const { email, password } = evt.target.elements;
+
+  if(!email.value || !password.value){
+    return alert("Fill in all fields");
+  }
+
+  console.log({
+    email: email.value,
+    password: password.value
+  });
+
+  formEl.reset()
+}
